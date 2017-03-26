03844e2e5cfc53a7e6bc623ec8d051cd0e4cb8e5
khanadakerakash <akash.cse10@gmail.com>
2017-03-26 14:57:50 +0600

    All task complete, but nodemon not working

---
+++ server.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Kh Akash on 3/26/2017.
+ */
+const express = require('express');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.registerPartials(__dirname + '/views/partials');
+
+/*hbs.registerHelper('creatorName', ()=> {
+   return "Kh Akash";
+});
+
+hbs.registerHelper('nowYears', () => {
+   return new Date().setFullYear();
+});*/
+
+app.get('/', (req,res) => {
+   res.render('home', {
+       page_name  : "Home page",
+       name       : "Home",
+       description: "home page bla bla bla..."
+   });
+});
+
+app.get('/', (req,res) => {
+    res.render('about', {
+        page_name  : "about page",
+        name       : "about",
+        description: "about page bla bla bla..."
+    });
+});
+
+app.get('/', (req,res) => {
+    res.render('work', {
+        page_name  : "work page",
+        name       : "work",
+        description: "work page bla bla bla..."
+    });
+});
+
+app.get('/', (req,res) => {
+    res.render('contact', {
+        page_name  : "contact page",
+        name       : "contact",
+        description: "contact page bla bla bla..."
+    });
+});
+
+app.listen(3000);
